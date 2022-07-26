7c105d1c03f246e9afcb8401e5911ff46ba1b37e
nathansandford <nathan_sandford@berkeley.edu>
2022-07-26 14:21:35 -0700

    separating likelihoods

---
+++ scripts/BinnedFitting.py
@@ -0,0 +1,101 @@
+from pathlib import Path
+import numpy as np
+import pandas as pd
+import multiprocess as mp
+import pocomc as pc
+from waf.par import DefaultParSet
+from waf.fitting.priors import UniformLogPrior, GaussianLogPrior
+from waf.fitting.pocomc_binned import log_prior
+from waf.fitting.pocomc_binned import log_likelihood
+from waf.fitting.pocomc_binned import log_probability
+from waf.fitting.pocomc_binned import ppc
+import matplotlib as mpl
+import matplotlib.pyplot as plt
+from corner import corner
+np.seterr(all="ignore");
+
+nwalkers = 5000
+data_file = Path('/global/scratch/users/nathan_sandford/ChemEv/EriII/data/EriII_MDF.dat')
+output_file = Path('/global/scratch/users/nathan_sandford/ChemEv/EriII/samples/Binned.npz')
+
+# Load Observed Data
+obs = pd.read_csv(data_file, index_col=0)
+obs_bins = np.linspace(-10, 2.0, 49)
+counts, obs_bins = np.histogram(obs['FeH'], bins=obs_bins, density=False)
+obs_mdf = {
+    'counts': counts,
+    'bins': obs_bins,
+}
+n_star = obs.shape[0]
+# Load Default Parameters
+par = DefaultParSet()
+par.t = np.arange(0.0001, 1.0001, 0.0001)
+mod_bins = np.linspace(-10, 2.0, 501)
+# Define Priors
+priors = dict(
+    logtauSFE=UniformLogPrior('logtauSFE', 0, 4, -np.inf),
+    tauSFH=GaussianLogPrior('tauSFH', 0.7, 0.2, 0, 1e2),
+    eta=UniformLogPrior('eta', 0, 1e3, -np.inf),
+    fRetCC=UniformLogPrior('fRetCC', 0, 1, -np.inf),  # GaussianLogPrior('fRetCC', 1, 0.3, 0, 1),
+    fRetIa=UniformLogPrior('fRetIa', 0, 1, -np.inf),  # GaussianLogPrior('fRetIa', 1, 0.3, 0, 1),
+)
+gal_par_names = list(gal_priors.keys())
+bounds = np.array([
+    [1e-2, 4],  # logtauSFE
+    [1e-2, 1e2],  # tauSFH
+    [0, 1e3],  # eta
+    [0, 1],  # fRetCC
+    [0, 1],  # fRetIa
+])
+# Initialize Walkers
+p0_list = []
+while len(p0_list) < nwalkers:
+    p = np.array(
+    [
+        2.0 + 1e-1 * np.random.randn(),  # logtauSFE
+        0.70 + 1e-1 * np.random.randn(),  # tauSFH
+        100 + 1e+1 * np.random.randn(),  # eta
+        0.8 + 1e-1 * np.random.randn(),  # fRetCC
+        0.8 + 1e-1 * np.random.randn(),  # fRetIa
+    ])
+    if fit_latent_FeH:
+        p = np.concatenate([p, [obs['FeH'][i] + obs['dFeH'][i] * np.random.randn() for i in range(n_star)]])
+    if np.isfinite(
+        log_probability(
+            p,
+            par,
+            priors,
+            gal_par_names,
+            obs=obs_mdf,
+        )
+    ):
+        p0_list.append(p)
+p0 = np.vstack(p0_list)
+nwalkers, ndim = p0.shape
+# Run PMC Sampling
+with mp.Pool(mp.cpu_count()) as pool:
+    # Sampler initialisation
+    sampler = pc.Sampler(
+        nwalkers,
+        ndim,
+        log_likelihood=log_likelihood,
+        log_likelihood_kwargs=dict(
+            default_par=par,
+            gal_par_names=gal_par_names,
+            obs=obs_mdf,
+        ),
+        log_prior=log_prior,
+        log_prior_kwargs=dict(
+            priors=priors,
+            gal_par_names=gal_par_names,
+        ),
+        bounds=bounds,
+        pool=pool,
+    )
+    # Run sampler
+    sampler.run(p0)
+    # Add extra samples
+    sampler.add_samples(9000)
+# Save Results
+results = sampler.results
+np.savez(output_file, **results)
--- waf/fitting/pocomc_binned.py
+++ waf/fitting/pocomc_binned.py
@@ -1,7 +1,7 @@
 import numpy as np
 from scipy.stats import poisson
 from waf.models import waf2017
-from waf.utils import get_MDF, get_PDF, eval_PDF
+from waf.utils import get_MDF
 
 
 def log_prior(p, priors, gal_par_names):
@@ -32,45 +32,6 @@ def log_probability(p, default_par, priors, gal_par_names, obs):
         return -np.inf
     else:
         return logP
-
-
-def log_prior_0bins(p, priors, gal_par_names, obs=None):
-    if p.ndim > 1:
-        raise AttributeError('log_prior is not vectorized')
-    p_gal = p[:len(gal_par_names)]
-    logPi = np.sum([priors[gal_par_names[i]](p_gal[i]) for i in range(len(gal_par_names))])
-    if obs is None:
-        p_star = p[len(gal_par_names):]
-        logPi += np.sum(priors['latent_FeH'](p_star))
-    return logPi
-
-
-def log_likelihood_0bins(p, default_par, gal_par_names, bins, obs=None):
-    if p.ndim > 1:
-        raise AttributeError('log_likelihood is not vectorized')
-    p_gal = p[:len(gal_par_names)]
-    p_star = p[len(gal_par_names):]
-    p_gal_dict = {par_name: p_gal[i] for i, par_name in enumerate(gal_par_names)}
-    default_par.update(p_gal_dict)
-    SFR, OH, FeH, OFe = waf2017(**default_par.__dict__)
-    FeH_PDF = get_PDF(FeH, SFR, bins)
-    if obs is None:
-        p_star = p[len(gal_par_names):]
-    else:
-        p_star = obs['FeH'].values
-    logL = eval_PDF(p_star, FeH_PDF, bins)
-    return logL
-
-
-def log_probability_0bins(p, default_par, priors, gal_par_names, bins, obs=None):
-    logPi = log_prior_0bins(p, priors, gal_par_names, obs)
-    logL = log_likelihood_0bins(p, default_par, gal_par_names, bins, obs)
-    logP = logPi + logL
-    if np.isnan(logP):
-        return -np.inf
-    else:
-        return logP
-
 
 def ppc(p_list, default_par, mod_bins, obs_mdf):
     if not isinstance(p_list, list):
