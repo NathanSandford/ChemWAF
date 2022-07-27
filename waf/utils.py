f39c10aed3b473e11004adb52261de5a752f9b9b
nathansandford <nathan_sandford@berkeley.edu>
2022-07-27 12:12:58 -0700

    randdist

--- waf/utils.py
+++ waf/utils.py
@@ -30,3 +30,23 @@ def eval_PDF(
     bin_centers = np.convolve(bins, np.ones(2), 'valid') / 2
     logL = np.sum(np.log(np.interp(obs, bin_centers, pdf)))
     return logL
+
+
+def randdist(x, pdf, nvals):
+    """Produce nvals random samples from pdf(x), assuming constant spacing in x."""
+    # get cumulative distribution from 0 to 1
+    if np.diff(x).max() -  np.diff(x).min() > 1e-10:
+        raise RuntimeError('x must have constant spacing')
+    cumpdf = np.cumsum(pdf)
+    cumpdf *= 1/cumpdf[-1]
+    # input random values
+    randv = np.random.uniform(size=nvals)
+    # find where random values would go
+    idx1 = np.searchsorted(cumpdf, randv)
+    # get previous value, avoiding division by zero below
+    idx0 = np.where(idx1==0, 0, idx1-1)
+    idx1[idx0==0] = 1
+    # do linear interpolation in x
+    frac1 = (randv - cumpdf[idx0]) / (cumpdf[idx1] - cumpdf[idx0])
+    randdist = x[idx0]*(1-frac1) + x[idx1]*frac1
+    return randdist
